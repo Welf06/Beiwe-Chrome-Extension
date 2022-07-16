def06f91f7737b471a606ae924b12a5bf0b31626
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-07-16 18:31:00 +0530

    login/logout logic written; setLabels logic;

--- js/script.js
+++ js/script.js
@@ -1,59 +1,102 @@
-/* 
-------------Front End---------------
-
--> working of extension:
-    > Keep the page from loading until API does its work
-    > fetch labels from server and check if the login session is active or not
-      > if not active then login and get the labels
-      > login => storing the email and password in local storage or letting the user input it  each time
-    > Get all images URL in the website and store it in an array
-    > send the img array to the backend to block the images along with the labels
-    > get back an array containing array of image urls to block 
-    > blur/remove the images
-    > displavy the website
-
--> features:
-    > Turning the extension on or off for the particular site or whole browser
-    > Login to the extension to maintain session to make API calls
-    > Setting, updating and Deleting the Labels
-    > An About page which takes user to the website of the extension
-
--> Issues/edge cases:
-    > Handling infinite scroll in the website like reddit or google images
-    
-*/
-
-// loading division
+//appending the loading division
 let loading_div = document.createElement('div');
 loading_div.innerHTML = `<div id="beiwe-loading-div">
 <img id="beiwe-loading-image" src="https://i.imgur.com/jFDZGw7.gif" alt="Loading..." />
 </div>`
 document.body.appendChild(loading_div);
 
-// removing the loading once the images are replaced with the blocked images
-$(window).on('load', function () {
-   $('#beiwe-loading-div').hide();
- }) 
+const email = 'neeraj.yathy@gmail.com';
+const password = '1234';
+
+
 
 // getting the images link 
+function getImageLinks(_callgetSessionKey) {
 const imageLinks = document.querySelectorAll('img');
 let imgArray = [];
-for (let i=0; i < imageLinks.length; i++) {
-    if (imageLinks[i].src !== "") {
-        imgArray.push(imageLinks[i].src);
-    }
+for (let i = 0; i < imageLinks.length; i++) {
+  if (imageLinks[i].src !== "") {
+    imgArray.push(imageLinks[i].src);
+  }
+}
+_callgetSessionKey();
+return imgArray;
+}
+
+let imgRequestData = {
+  "data": {
+    "img_urls": getImageLinks(getSessionKey),
+    "email": email,
+    // "session_key": token,
+    // "website": window.location.href
+  }
+}
+
+// getting the session key for making requests to the backend
+function getSessionKey() {
+let token = "";
+chrome.storage.sync.get("session_key", (obj) => {
+  let session_key = obj.session_key;
+  if (session_key === undefined) {
+    session_key = "";
+  }
+  // console.log(`got session key {${session_key}}`); //works
+  token = session_key;
+  imgRequestData.data.session_key = token;
+  imgRequestData.data.website = window.location.href;
+  // console.log(imgRequestData); //works
+  const imgsToBlockArray = imgsToBlock(imgRequestData.data.img_urls, imgRequestData, function (array) {
+    blockImages(array, removeLoadingDiv);
+    console.log(array);
+    });
+  // console.log(imgArray); //works 
+});
+}
+
+// removing the loading once the blocked images are hidden
+function removeLoadingDiv() {
+  document.body.removeChild(loading_div);
 }
 
 // getting the urls to block from the server
-// if not logged in then login and get the urls
-function imgsToBlock(array){
-  return array;
+function imgsToBlock(array, data, _callback) {
+  let toBlock = [];
+  chrome.storage.sync.get("login_status", function (obj) {
+    let login_data = obj.login_status;
+    if (login_data === "logged in") {
+      console.log("retrieving urls to block from server");
+      $.ajax({
+        type: "GET",
+        url: `https://beiwe.herokuapp.com/extension/api_call?images=` + encodeURIComponent(JSON.stringify(data)),
+        success: function (data) {
+          console.log(data);
+          data = JSON.parse(data);
+          if (data.status === "Image labels not returned") {
+            alert(`You are not logged in to Beiwe backend. Please login to Beiwe backend and try again.`);
+            // imgsToBlock(array);
+          }
+          else {
+            let blockList = data.blocklist;
+            for (let i = 0; i < array.length; i++) {
+              for (let j = 0; j < blockList.length; j++) {
+                if (array[i] === blockList[j]) {
+                  toBlock.push(array[i]);
+                }
+              }
+            }
+          }
+
+        }
+      });
+    }
+  });
+  _callback(toBlock);
+  return toBlock;
 }
 
-// for now just checking each img to find if it matches the url
-const toBlock = imgsToBlock(imgArray);
 
-toBlock.forEach(
+function blockImages(toBlock, removeLoadingDiv) {
+  toBlock.forEach(
     function (img) {
       imageLinks.forEach(
         function (imgLink) {
@@ -63,60 +106,9 @@ toBlock.forEach(
         }
       )
     }
-);
-
-const email = 'neeraj.yathy@gmail.com';
-const password = '1234';
-
-loginData = {
-  "data": {
-    "email": email,
-    "password": password
-  }
+  );
+  removeLoadingDiv();
 }
 
-// send post HTTP request to https://beiwe.herokuapp.com/extension/login to login
-$.ajax({
-  type: "POST",
-  url: `https://beiwe.herokuapp.com/extension/login?login_data=` + encodeURIComponent(JSON.stringify(loginData)),
-  // contentType: "application/json",
-  success: function(data) {
-    setLabel();
-    console.log(data);
-  }
-});
 
-// setting the labels
-const labels = ['covid-19', 'coronavirus', 'car', 'clown', 'cross'];
-const labelData = {
-  "data": {
-    "labels": labels
-}
-}
 
-// sending the labels to the server
-function setLabel() {
-$.ajax({
-  type: "POST",
-  url: `https://beiwe.herokuapp.com/extension/setlabel?labels=` + encodeURIComponent(JSON.stringify(labelData)),
-  data: JSON.stringify(labelData),
-  contentType: "application/json",
-  success: function(data) {
-    console.log(data);
-  }
-});
-}
-// const setLabelURL =  `https://beiwe.herokuapp.com/extension/setlabel?labels=` + encodeURIComponent(JSON.stringify(labelData));
-// fetch(setLabelURL)
-// .then(response => response)
-// .then(data => console.log(data))
-/*
-{
-    "labels": [],
-    "params": [
-        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-    ]
-} 
-*/

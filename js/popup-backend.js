beeac7334031823bfdf48fb5f81ecb1dc2170a4e
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-10-01 20:47:08 +0530

    onclick events added
    login and logout buttons fixed

--- js/popup-backend.js
+++ js/popup-backend.js
@@ -1,147 +1,198 @@
 // contains 
 //    login/logout logic [done]
+//    signup logic [done]
 //    sending the labels to the server [done]
-//    updating labels 
-//    deleting labels
-const loginButton = document.querySelector("#login-button");
-const logoutButton = document.querySelector("#logout-button");
-const setLabelButton = document.querySelector("#setlabel-button");
-chrome.storage.sync.set({"login_status" : "logged out" });
-
+const loginButton = document.getElementById("login-button");
+const logoutButton = document.getElementById("logout-button");
+const signupbutton = document.getElementById("sign-up");
+const setlabelbutton = document.getElementById("setlabel");
 // API calls
-const email = 'neeraj.yathy@gmail.com';
-const password = '1234';
+//const email = 'neeraj.yathy@gmail.com';
+//const password = '1234';
+//const username = "nxpy";
 let token = "";
-chrome.storage.sync.get("session_key", (obj) => {
-   let session_key = obj.session_key;
-   if (session_key === undefined) {
-      session_key = "";
-      }
-   token = session_key;
+let email = "";
+let password = "";
+let username = "";
+
+let labels = ["cat", "goat"];
+
+//add an element to the array
+function addElement() {
+    labels.push(document.getElementById('word').value);
+    console.log(labels);
+}
+
+function show() {
+    var x = document.getElementsByClassName("hi");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "block";
+    }
+}
+
+function hide() {
+    var x = document.getElementsByClassName("hi");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+}
+
+loginButton.addEventListener('click', () => {
+    email = document.getElementById("mail").value;
+    password = document.getElementById("password").value;
+    let loginData = {
+        "data": {
+            "email": email,
+            "password": password,
+        }
+    }
+    login(loginData);
 });
 
-let loginData = {
-  "data": {
-    "email": email,
-    "password": password,
-    "returnSecureToken" : "true"
-  }
-}
+logoutButton.addEventListener('click', () => {
+    let logoutData = {
+        "data": {
+            "email": email,
+            "session_key": token
+        }
+    }
+    logout(logoutData);
+});
 
-let logoutData = {
-  "data": {
-    "email": email,
-    "session_key": token
-  }
-}
+signupbutton.addEventListener('click', () => {
+    username = document.getElementById("username").value;
+    email = document.getElementById("mail").value;
+    password = document.getElementById("password").value;
+    let signupdata = {
+        "data": {
+            "email": email,
+            "name": username,
+            "password": password
+        }
+    }
+    signup(signupdata);
+});
 
-const labels = ['balls','covid-19', 'coronavirus', 'car', 'clown', 'cross', 'class', 'Dog'];
-const labelData = {
-"data": {
-   "labels": labels,
-   "email": email,
-   "session_key": token
-}
-}
+//something wrong with this button
+setlabelbutton.addEventListener('click', () => {
+    window.alert("set label button clicked");
+    let labelData = {
+        "data": {
+            "labels": labels,
+            "email": email,
+            "session_key": token
+        }
+    }
+    setLabel(labelData);
+});
 
-loginButton.addEventListener("click", () => {
-   console.log(`clicked login button`);
-   login(loginData);
-   });
-
-logoutButton.addEventListener("click", () => {
-   console.log(`clicked logout button`);
-   logout(logoutData);
-   });
-
-setLabelButton.addEventListener("click", () => {
-   console.log(`clicked set label button`);
-   setLabel(labelData);
-   });
 
 // send post HTTP request to https://beiwe.herokuapp.com/extension/login to login
+// send get HTTP request to https://beiwe.herokuapp.com/extension/setlabel to set the labels
+// send post HTTP request to https://beiwe.herokuapp.com/extension/logout to logout
 function login(loginData) {
-   chrome.storage.sync.get("login_status", function (obj) {
-      let login_data = obj.login_status;
-      if (login_data === "logged out") {
-         $.ajax({
+    chrome.storage.sync.get("login_status", function(obj) {
+        let login_data = obj.login_status;
+        if (login_data === "Logged in") {
+            window.alert("Already logged in");
+            return;
+        }
+        $.ajax({
             type: "POST",
             url: `https://beiwe.herokuapp.com/extension/login?login_data=` + encodeURIComponent(JSON.stringify(loginData)),
-            // contentType: "application/json",
+            contentType: "application/json",
             success: function(data) {
-               data = JSON.parse(data);
-               if (data.status === "Logged in"){
-               console.log(`logged in to beiwe backened`)
-               const session_key = data.session_key;
-               chrome.storage.sync.set({ "session_key": session_key }); 
-               console.log(`session key set`);
-               chrome.storage.sync.set({"login_status" : "logged in" });
-               token = session_key;
-               labelData.data.session_key = session_key;
-               logoutData.data.session_key = session_key;
-               }
-              else if (data.status === "Not logged in"){
-                  console.log(`not logged in`);
-               }
+                data = JSON.parse(data);
+                if (data.status === "Logged in") {
+                    window.alert("Login sucessfull");
+                    const session_key = data.session_key;
+                    chrome.storage.sync.set({ "session_key": session_key });
+                    console.log(`session key set`);
+                    chrome.storage.sync.set({ "login_status": "logged in" });
+                    token = session_key;
+                    hide();
+                    document.getElementById('logout-button').style.display = "block";
+                } else if (data.status === "Not logged in") {
+                    window.alert("Login unsucessfull");
+                }
             }
-          });
-         }
-         else if (login_data == "logged in") {
-          console.log(`already logged in to beiwe backend`);
-         }
-  });
+        });
+    });
 }
 
 // sending the labels to the server
 function setLabel(labelData) {
-   chrome.storage.sync.get("login_status", function (obj) {
-      let login_data = obj.login_status;
-      if (login_data === "logged in") {
-         $.ajax({
-         type: "GET",
-         url: `https://beiwe.herokuapp.com/extension/setlabel?labels=` + encodeURIComponent(JSON.stringify(labelData)),
-         data: JSON.stringify(labelData),
-         contentType: "application/json",
-         success: function(data) {
-            console.log(data);
-            data = JSON.parse(data);
-            if (data.status === "user not logged in"){
-               login(loginData);
-               setLabel(labelData);
+    window.alert("gota here");
+    chrome.storage.sync.get("login_status", function(obj) {
+        let login_data = obj.login_status;
+        if (login_data === "logged out") {
+            window.alert("Please login first");
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: `https://beiwe.herokuapp.com/extension/setlabel?setlabel_data=` + encodeURIComponent(JSON.stringify(labelData)),
+            contentType: "application/json",
+            success: function(data) {
+                console.log(data);
+                data = JSON.parse(data);
+                if (data.status === "labels updated/inserted") {
+                    window.alert("Labels inserted");
+                    clearInput();
+                } else if (data.status === "Error") {
+                    window.alert("Error");
+                }
             }
-            else if (data.status === "labels updated/inserted"){
-               console.log(`labels set`);
-               }
-         }
-         });
-   }
-   else if (login_data == "logged out") {
-      console.log(`not logged in to beiwe backend`);
-   }
-});
+        });
+    });
 }
 
-function logout(logoutData) {
-   chrome.storage.sync.get("login_status", function (obj) {
-      let login_data = obj.login_status;
-      if (login_data === "logged in") {
-         $.ajax({
-            type: "GET",
-            url: `https://beiwe.herokuapp.com/extension/logout?logout_data=`+ encodeURIComponent(JSON.stringify(logoutData)),
-            success: function(data) {
-               console.log(data);
-               data = JSON.parse(data);
-               if (data.status === "Not logged out"){
-                  console.log(`not logged out`);
-               }
-               else if (data.status === "logged out"){
-                  chrome.storage.sync.set({"login_status" : "logged out" });
-               }
+
+//send signup details to the backend in heroku backend receive the conformation from backend
+function signup(signupdata) {
+    $.ajax({
+        type: "POST",
+        url: `https://beiwe.herokuapp.com/extension/signup?signup_data=` + encodeURIComponent(JSON.stringify(signupdata)),
+        contentType: "application/json",
+        success: function(data) {
+            console.log(data);
+            data = JSON.parse(data);
+            window.alert(data.status);
+            if (data.status === "User created") {
+                console.log(`signed up to beiwe backened`);
+                window.alert("signup successful");
+            } else if (data.status === "User not created") {
+                console.log(`not signed up`);
             }
-            });
-         }
-      else if (login_data == "logged out") {
-         console.log(`already logged out from beiwe backend`);
-      }
-});
+        }
+    });
+
+}
+
+//logout function
+function logout(logoutData) {
+    chrome.storage.sync.get("login_status", function(obj) {
+        let login_data = obj.login_status;
+        if (login_data === "logged out") {
+            window.alert("Already logged out");
+            return;
+        }
+        $.ajax({
+            type: "GET",
+            url: `https://beiwe.herokuapp.com/extension/logout?logout_data=` + encodeURIComponent(JSON.stringify(logoutData)),
+            success: function(data) {
+                console.log(data);
+                data = JSON.parse(data);
+                if (data.status === "Not logged out") {
+                    console.log(`not logged out`);
+                } else if (data.status === "logged out") {
+                    chrome.storage.sync.set({ "login_status": "logged out" });
+                    clearInput();
+                    show();
+                    document.getElementById('logout-button').style.display = "none";
+                    window.alert("logged out");
+                }
+            }
+        });
+    });
 }
